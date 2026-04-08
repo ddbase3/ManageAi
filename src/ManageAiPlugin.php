52c9fac2301f915c00cd2c0450ffc6c3ec761d52
Daniel Dahme <danieldahme@googlemail.com>
2026-04-08 14:57:48 +0000

    settingsstore added

--- src/ManageAiPlugin.php
+++ src/ManageAiPlugin.php
@@ -3,6 +3,9 @@
 namespace ManageAi;
 
 use Base3\Api\IContainer;
+use Base3\Configuration\Api\IConfiguration;
+use Base3\Settings\Api\ISettingsStore;
+use Base3\Settings\Json\JsonSettingsStore;
 use Base3Manager\Plugin\AbstractPlugin;
 
 class ManageAiPlugin extends AbstractPlugin {
@@ -11,7 +14,9 @@ class ManageAiPlugin extends AbstractPlugin {
 
 	public function init() {
 		$this->container
-			->set(self::getName(), $this, IContainer::SHARED);
+			->set(self::getName(), $this, IContainer::SHARED)
+
+			->set(ISettingsStore::class, fn($c) => new JsonSettingsStore($c->get(IConfiguration::class)), IContainer::SHARED);
 	}
 
 	// Implementation of ICheck
