5b041e3dd3453b2a08cb76372da86e51933fc5ae
Daniel Dahme <danieldahme@googlemail.com>
2026-06-10 20:29:57 +0000

    more agent displays added

---
+++ tpl/ContentControl/AdministrationAgentToolTestContentControl.php
@@ -0,0 +1,5 @@
+<div id="AdministrationAgentToolTestContentControl" class="contentcontrol">
+        <div id="wrap">
+		<?php echo $this->_['content']; ?>
+	</div>
+</div>
---
+++ tpl/ContentControl/AdministrationUserPrefDefContentControl.php
@@ -0,0 +1,5 @@
+<div id="AdministrationUserPrefDefContentControl" class="contentcontrol">
+        <div id="wrap">
+		<?php echo $this->_['content']; ?>
+	</div>
+</div>
